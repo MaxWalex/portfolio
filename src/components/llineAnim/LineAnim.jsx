cad863f8fe127f1f4e36727f4573f28b36a180c6
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2023-02-08 16:28:15 +0200

    anim in the frist screen

---
+++ src/components/llineAnim/LineAnim.jsx
@@ -0,0 +1,14 @@
+import './lineAnim.scss';
+
+function LineAnim() {
+
+  return (
+    <div class="mouse">
+       <div className="ring"></div>
+       <div className="ring"></div>
+       <div className="ring"></div>
+    </div>
+  );
+}
+
+export default LineAnim;
