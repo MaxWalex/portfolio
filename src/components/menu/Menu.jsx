f693e728e0d55fe1dfbfc8a386eddfba988d0af3
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2023-02-08 16:28:25 +0200

    added anim

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-
 import Links from './Links'
-import CheckboxTheme from '../checkboxTheme/CheckboxTheme'
+import CheckboxTheme from '../checkboxTheme/CheckboxTheme';
 import './menu.scss'
 
 function Menu() {
+
   return (
     <nav>
         <CheckboxTheme />
+
         <ul className='menu_items'>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Skills</a></li>
-          <li><a href="#">Work</a></li>
-          <li><a href="#">Contacts</a></li>
+          <li><a href="#">About
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            </a>
+          </li>
+          <li><a href="#">Works
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            </a>
+          </li>
+          <li><a href="#">Contacts
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            </a>
+          </li>
         </ul>
         <Links />
     </nav>
