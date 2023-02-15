a6ee84b7625528a016bccf344fccd3192335f526
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2023-02-15 17:39:10 +0200

    mouse context

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,6 @@
+// import { BrowserRouter as Router, Route ,Link, Routes } from "react-router-dom";
+
+import MouseContextProvider from '../../context/MouseContext';
 import FullPageScroll from '../fullPageScroll/FullPageScroll';
 
 import './App.scss';
@@ -5,7 +8,9 @@ import './App.scss';
 function App() {
 
   return (
-    <FullPageScroll />
+    <MouseContextProvider>
+      <FullPageScroll />
+    </MouseContextProvider>
   );
 }
 
